a9edf2b13b595880a0374e47e9b25fd8ce6b51c8
Federico <federicobarbagallo@gmail.com>
2022-06-20 19:13:24 -0300

    Basic Folder and Component Structure

---
+++ src/info/services.js
@@ -0,0 +1,14 @@
+const services = [
+  {
+    id: 1,
+    title: "web development",
+    text: `lorem`,
+  },
+  {
+    id: 2,
+    title: "web design",
+    text: `lorem.`,
+  },
+];
+
+export default services;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
-import React from "react"
+import React from "react";
 
 export default function Home() {
-  return <div>Portfolio Goes here!</div>
+  return (
+    <>
+      <main>Portfolio In Progress!</main>
+    </>
+  );
 }
