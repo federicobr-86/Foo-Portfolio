57f3bb8ae804012a4b7b75fc5e390ced38745e21
Federico <federicobarbagallo@gmail.com>
2022-06-20 19:30:35 -0300

    About Page Added

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <article className="about-text">
+            <p>About Me</p>
+            <div className="about-stack">Tech Stack...</div>
+          </article>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
