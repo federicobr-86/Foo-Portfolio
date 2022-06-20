cb4234c0dc77c4c47128acbdad1987f5927458b2
Federico <federicobarbagallo@gmail.com>
2022-06-20 19:21:36 -0300

    Basic Main Page Working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <>
-      <main>Portfolio In Progress!</main>
+      <Hero />
+      <main></main>
     </>
   );
 }
