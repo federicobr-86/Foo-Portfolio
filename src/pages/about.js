82469aa3a9dc53509d81db0578ce044b17993bf7
Federico <federicobarbagallo@gmail.com>
2022-06-20 20:01:24 -0300

    Title Component Added

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Title from "../components/Title";
 
 const About = () => {
+  const title = "About me!";
+
   return (
     <>
       <section className="about-page">
         <div className="section-center about-center">
           <article className="about-text">
-            <p>About Me</p>
+            <Title title={title} />
+            <p>In progress</p>
             <div className="about-stack">Tech Stack...</div>
           </article>
         </div>
