6982485adcb635ccc126936c991980bfad9f4505
Federico <federicobarbagallo@gmail.com>
2022-06-21 17:47:30 -0300

    About Image Added

--- src/info/services.js
+++ src/info/services.js
@@ -1,11 +1,16 @@
+import React from "react";
+import { FaCode, FaUserTie } from "react-icons/fa";
+
 const services = [
   {
     id: 1,
+    icon: <FaCode className="service-icon" />,
     title: "Frontend Web Developer",
     text: `(Experience info)`,
   },
   {
     id: 2,
+    icon: <FaUserTie className="service-icon" />,
     title: "Engineering Project Management",
     text: `(Experience info).`,
   },
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Title from "../components/Title";
+import aboutImg from "../assets/images/product.svg";
 
 const About = () => {
   const title = "About me!";
@@ -10,6 +11,7 @@ const About = () => {
         <div className="section-center about-center">
           <article className="about-text">
             <Title title={title} />
+            <img src={aboutImg} alt="about me" className="about-img-svg" />
             <p>Coming soon!</p>
             <div className="about-stack">Tech Stack info goes here...</div>
           </article>
