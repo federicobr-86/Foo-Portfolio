1f1e74ef802112a443cf0a92a8d6cc3f2b4519e7
Federico <federicobarbagallo@gmail.com>
2022-08-04 15:15:49 -0300

    Social Links Icon css fix

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,20 +5,20 @@ import social from "../../data/social_links";
 import text from "../../data/text";
 
 const Hero = () => {
-  const { hero_title, hero_description, hero_contact } = text;
+  const { hero_greeting, hero_title, hero_description, hero_contact } = text;
 
   return (
     <header className="hero">
       <section className="section-center hero-center">
         <article className="hero-info">
-          <div>
-            <div className="underline"></div>
-            <h1>{hero_title}</h1>
-            <h4>{hero_description}</h4>
-            <Link to="/contact" className="btn">
-              {hero_contact}
-            </Link>
-            <div className="social-links">
+          <h3>{hero_greeting}</h3>
+          <h2>{hero_title}</h2>
+          <h2>{hero_description}</h2>
+          <Link to="/contact" className="btn">
+            {hero_contact}
+          </Link>
+          <div className="hero-centered">
+            <div className="hero-social">
               {social.map((link) => {
                 return (
                   <a href={link.url} key={link.id} className="social-link">
--- src/data/text.js
+++ src/data/text.js
@@ -39,7 +39,8 @@ const text = {
   skills_stack_git: "GIT",
   skills_stack_scrum: "Scrum",
   skills_stack_project: "Project Management",
-  hero_title: "Hi! I'm Federico",
+  hero_greeting: "Hello!",
+  hero_title: "I'm Federico",
   hero_description: "Frontend Developer",
   hero_contact: "Contact Me!",
 };
