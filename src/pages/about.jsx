693069477b4883f1bf952f9c813f5a5207108239
Federico <federicobarbagallo@gmail.com>
2022-08-04 12:17:54 -0300

    About - with new text data import

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import Title from "../components/Title/index";
+import text from "../data/text";
 import aboutImg from "../assets/images/about.jpg";
 
 const About = () => {
-  const title = "About me!";
-  const intro =
-    "Passionate about programming languages and computers since 1995.";
-  const experienced =
-    "Since then, I became a highly motivated engineer with more than 10 years of experience in large scale projects.";
-  const now = "Currently working with mainly with React, Gatsby and CMS.";
+  const { about_title, about_intro, about_experience, about_currently } = text;
+  const title = `${about_title}`;
 
   return (
     <>
       <section className="section projects">
         <div className="section-center projects-center">
-          <Title title={title} />
-          <p>{intro}</p>
-          <p>{experienced}</p>
+          <Title title={about_title} />
+          <p>{about_intro}</p>
+          <p>{about_experience}</p>
           <img src={aboutImg} alt={title} className="about-img" />
-          <p>{now}</p>
+          <p>{about_currently}</p>
         </div>
       </section>
     </>
