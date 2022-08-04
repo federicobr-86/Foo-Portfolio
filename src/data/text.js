2db90d99d970f15a57fead2455787af337c676cc
Federico <federicobarbagallo@gmail.com>
2022-08-04 12:17:32 -0300

    Text Data Object

---
+++ src/data/text.js
@@ -0,0 +1,13 @@
+//to be added to CMS in the near future
+const text = {
+  not_found: "Page not found!",
+  go_back: "Let's go back home!",
+  about_title: "About me!",
+  about_intro:
+    "Passionate about programming languages and computers since 1995.",
+  about_experience:
+    "Since then, I became a highly motivated engineer with more than 10 years of experience in large scale projects.",
+  about_currently: "Currently working mainly with React, Gatsby and CMS.",
+};
+
+export default text;
