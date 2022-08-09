6fdb188dd78a5c4043b9362df8166766e7d98b8a
Federico <federicobarbagallo@gmail.com>
2022-08-09 08:42:51 -0300

    Material UI Install #3

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-material-ui`,
     `gatsby-transformer-remark`,
     {
       resolve: "gatsby-plugin-anchor-links",
