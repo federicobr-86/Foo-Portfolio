e88e75ed8bc6b6ccc3760c7d6088315ad1b3bb8c
Federico <federicobarbagallo@gmail.com>
2022-08-15 09:36:14 -0300

    AboutMe Query Update #15

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -3,19 +3,19 @@ import { graphql, useStaticQuery } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Title from "../Title";
 
-const query = graphql`
+export const query = graphql`
   {
-    allContentfulAboutMe(sort: { fields: contentful_id }) {
-      nodes {
-        id
-        aboutText
-      }
-    }
-    contentfulAboutTitle {
+    contentfulTitles {
       aboutTitle
     }
-    contentfulAboutImage {
-      myImage {
+    allContentfulAboutMe(sort: { fields: contentful_id }) {
+      nodes {
+        aboutText
+        id
+      }
+    }
+    contentfulImages {
+      aboutImage {
         gatsbyImageData(height: 480)
       }
     }
@@ -25,12 +25,12 @@ const query = graphql`
 function AboutMe() {
   const data = useStaticQuery(query);
   const {
+    contentfulTitles: { aboutTitle },
     allContentfulAboutMe: { nodes: aboutText },
-    contentfulAboutTitle: { aboutTitle },
-    contentfulAboutImage: { myImage },
+    contentfulImages: { aboutImage },
   } = data;
 
-  const aboutImage = getImage(myImage);
+  const myImage = getImage(aboutImage);
 
   return (
     <main>
@@ -40,7 +40,7 @@ function AboutMe() {
           {aboutText.map((item) => {
             return <p key={item.id}>{item.aboutText}</p>;
           })}
-          <GatsbyImage image={aboutImage} alt="portfolio image of federico" />
+          <GatsbyImage image={myImage} alt="portfolio image of federico" />
         </div>
       </section>
     </main>
